1742fa0a16275dd90e6c65154f11267c62791079
yoonemong <yoonemong@naver.com>
2021-01-11 08:27:35 +0900

    refactring

--- VUE-INTERMEDIATE/vue-todo/src/App.vue
+++ VUE-INTERMEDIATE/vue-todo/src/App.vue
@@ -1,8 +1,8 @@
 <template>
   <div id="app">
     <TodoHeader></TodoHeader>
-    <TodoInput></TodoInput>
-    <TodoList></TodoList>
+    <TodoInput v-on:addTodoItem="addOneItem"></TodoInput>
+    <TodoList v-bind:propsdata="todoItems"></TodoList>
     <TodoFooter></TodoFooter>
   </div>
 </template>
@@ -14,6 +14,27 @@ import TodoList from './components/TodoList'
 import TodoFooter from './components/TodoFooter'
 
 export default {
+  data:function(){
+    return {
+      todoItems : []
+    }
+  },
+  methods:{
+    addOneItem:function(todoItem){
+        var obj = {completed:false , item:todoItem};
+        localStorage.setItem(todoItem,JSON.stringify(obj));
+        console.log(todoItem);
+        this.todoItems.push(obj);
+    }
+  },
+  created: function(){
+     if(localStorage.length > 0){
+       for(var i=0;i<localStorage.length;i++){
+         if(localStorage.key(i) !== 'loglevel:webpack-dev-server')
+            this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+       }
+     }
+   },
   components:{
     'TodoHeader':TodoHeader,
     'TodoInput':TodoInput,
